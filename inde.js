99b97c1cb5110c1fc663c1fc6ed080cdcc889e3b
HimanshuBisen1799 <124518607+HimanshuBisen1799@users.noreply.github.com>
2023-10-16 22:28:22 +0530

    Update inde.js

--- inde.js
+++ inde.js
@@ -1,90 +1,85 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    //  sticky navbar on scroll script  //
-    if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    // Sticky navbar on scroll script
+    if (window.scrollY > 20) {
+      document.querySelector(".navbar").classList.add("sticky");
     } else {
-      $(".navbar").removeClass("sticky");
+      document.querySelector(".navbar").classList.remove("sticky");
     }
 
-    //  scroll-up button show/hide script  //
-    if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
+    // Scroll-up button show/hide script
+    if (window.scrollY > 500) {
+      document.querySelector(".scroll-up-btn").classList.add("show");
     } else {
-      $(".scroll-up-btn").removeClass("show");
+      document.querySelector(".scroll-up-btn").classList.remove("show");
     }
   });
 
-  //  slide-up script  //
-
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-    //  removing smooth scroll on slide-up button click  //
-    $("html").css("scrollBehavior", "auto");
+  // Slide-up script
+  document.querySelector(".scroll-up-btn").addEventListener("click", function () {
+    document.documentElement.scrollTo({ top: 0, behavior: "smooth" });
   });
 
-  $(".navbar .menu li a").click(function () {
-    //  Smooth scroll on Menu Items click  //
-
-    $("html").css("scrollBehavior", "smooth");
+  document.querySelectorAll(".navbar .menu li a").forEach(function (link) {
+    // Smooth scroll on Menu Items click
+    link.addEventListener("click", function () {
+      document.documentElement.style.scrollBehavior = "smooth";
+    });
   });
 
-  //  Toggle Navbar  //
-
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
+  // Toggle Navbar
+  document.querySelector(".menu-btn").addEventListener("click", function () {
+    document.querySelector(".navbar .menu").classList.toggle("active");
+    document.querySelector(".menu-btn i").classList.toggle("active");
   });
 
-  //  Typing Text Animation  //
-
-  var typed = new Typed(".typing", {
+  // Typing Text Animation
+  var typedOptions = {
     strings: [
       "Fullstack Developer",
       "Software Developer",
       "Python Developer",
       "Founder",
-      "Author"
+      "Author",
     ],
     typeSpeed: 100,
     backSpeed: 60,
-    loop: true
-  });
-
-  var typed = new Typed(".typing-2", {
-    strings: [
-      "Fullstack Developer",
-      "Software Developer",
-      "Python Developer",
-      "Founder",
-      "Author"
-    ],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
-
-  //  Owl Carousel  //
-
-  $(".carousel").owlCarousel({
-    margin: 20,
     loop: true,
-    autoplay: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false
+  };
+
+  new Typed(".typing", typedOptions);
+  new Typed(".typing-2", typedOptions);
+
+  // Owl Carousel
+  const carousel = document.querySelector(".carousel");
+  if (carousel) {
+    new OwlCarousel(carousel, {
+      margin: 20,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false,
+        },
+        600: {
+          items: 2,
+          nav: false,
+        },
+        1000: {
+          items: 3,
+          nav: false,
+        },
       },
-      600: {
-        items: 2,
-        nav: false
-      },
-      1000: {
-        items: 3,
-        nav: false
-      }
-    }
-  });
+    });
+  }
 });
+
+// Owl Carousel class
+class OwlCarousel {
+  constructor(element, options) {
+    // Initialize Owl Carousel here
+  }
+}
